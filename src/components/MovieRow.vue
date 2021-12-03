be2ea1f559c6eb7bf6fcb87fa4259dc4eda0f453
Shiela  Escultura <shielaescultura@gmail.com>
2021-12-03 19:42:16 +0800

    feat: add vue-awesome-swiper.

--- src/components/MovieRow.vue
+++ src/components/MovieRow.vue
@@ -1,22 +1,64 @@
+
 <template>
-  <div>
-    <h1>hello</h1>
-    <pre>
-      {{movies}}
-    </pre>
+<div class="MovieSlider__wrapper">
+  <div class="example-3d">
+    <swiper class="swiper" :options="swiperOption">
+        <swiper-slide v-for="(movie) in movies" :key="movie.id.label">
+          <div class="MovieCard" @click="view(movie)">
+            <div class="MovieCard___img">
+              <img :src="movie['im:image'][2]['label']" style="height:400px;width:300px;object-fit:cover;"/>
+              <h6 style="color:black;position:absolute;top:8px;left:16px;background-color:white;">
+                {{movie.title.label}}
+              </h6>
+            </div>
+          </div>
+        </swiper-slide>
+      <div class="swiper-pagination" slot="pagination" style="display:none;"></div>
+    </swiper>
+    <br>
   </div>
+</div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+
 export default {
+  name: 'swiper-example-3d-coverflow',
+  title: '3D Coverflow effect',
+  components: {
+    Swiper,
+    SwiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true
+        },
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      }
+    }
+  },
   created () {
     this.$store.dispatch('movies/loadMovies')
     console.log('created')
   },
   methods: {
-    // view (movie) {
-    //   console.log('movie', movie)
-    // }
+    view (movie) {
+      console.log('movie', movie)
+    }
   },
   computed: {
     movies () {
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,27 @@
 <template>
-  <q-page class="flex">
-    <MovieRow/>
-    <!-- <h1>hello</h1> -->
+  <q-page>
+    <div class="Home">
+      <div class="Home__slider-list">
+        <MovieRow/>
+      </div>
+    </div>
+    <br>
+    <!-- <TopMovies/> -->
+    <br>
+    <!-- <Feature/> -->
   </q-page>
 </template>
 
 <script>
+// import TopMovies from '../components/TopMovies.vue'
 import MovieRow from '../components/MovieRow.vue'
+// import Feature from '../components/Feature.vue'
 export default {
+  name: 'PageIndex',
   components: {
+    // TopMovies,
     MovieRow
+    // Feature
   }
 }
 </script>
